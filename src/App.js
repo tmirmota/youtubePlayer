442bb007fe9387f257c8fad613ed52847bee2047
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2017-04-03 01:07:37 -0700

    Added list of videos and the option to select a video

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
 // Components
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 
 const API_KEY = 'AIzaSyCF-2OZnyKEundx-P1V8KnSvMM5aVLz4XU';
 
@@ -9,17 +11,23 @@ class App extends Component {
   constructor() {
     super();
     YTSearch({ key: API_KEY, term: 'justice' }, (videos) => {
-      this.setState({ videos })
+      this.setState({ videos, selectedVideo: videos[0] })
     });
   }
   state = {
-    videos: []
+    videos: [],
+    selectedVideo: null
   }
+
 
   render() {
     return (
       <div>
         <SearchBar className="input-group" />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoClick={selectedVideo => this.setState({selectedVideo})} />
       </div>
     );
   }
---
+++ src/components/VideoList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// Components
+import VideoListItem from './VideoListItem';
+
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return (
+      <VideoListItem
+        key={video.etag}
+        video={video}
+        onVideoClick={props.onVideoClick} />
+    );
+  });
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+    </ul>
+  );
+}
+
+export default VideoList;
