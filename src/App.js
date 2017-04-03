51f2adcdbd877a21be4bdf35a262c4c9ff8ffa10
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2017-04-02 23:05:00 -0700

    Add list of Youtube objects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-
+import YTSearch from 'youtube-api-search';
 // Components
 import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyCF-2OZnyKEundx-P1V8KnSvMM5aVLz4XU';
 
 class App extends Component {
+  constructor() {
+    super();
+    YTSearch({ key: API_KEY, term: 'justice' }, (videos) => {
+      this.setState({ videos })
+    });
+  }
+  state = {
+    videos: []
+  }
+
   render() {
     return (
       <div>
