f003d8b8064b76633bd90031b7bae70548467fdb
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2017-04-03 22:30:39 -0700

    finished react app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 // css
 import './css/App.css';
 // Components
@@ -27,11 +28,12 @@ class App extends Component {
 
 
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return (
       <div>
-        <SearchBar 
+        <SearchBar
           className="input-group"
-          onSearch={term => this.videoSearch(term)} />
+          onSearch={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           videos={this.state.videos}
